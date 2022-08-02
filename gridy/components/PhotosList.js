53090e66ac8b607b826dacc9a4fcdff84f8ba649
Mustafa Nawzat <mustafanawzatt@gmail.com>
2022-08-03 01:41:16 +0300

    feat: config InfiniteScroll with PhotosList com.

--- gridy/components/PhotosList.js
+++ gridy/components/PhotosList.js
@@ -1,22 +1,59 @@
 import React, { useState, useEffect } from "react";
 import { Container } from "react-bootstrap";
 import Image from "next/image";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchPhotos } from "../pages/api/unsplash";
 
 function PhotosList({ results, total, error }) {
   const [photos, setPhotos] = useState(results);
+  const [page, setPage] = useState(2);
+  const [hasMore, setHasMore] = useState(true);
+
+  useEffect(() => {
+    if (photos && total) {
+      setHasMore(total > photos.length ? true : false);
+    }
+  }, [photos, total, error]);
+
+  const fetchNextPage = async () => {
+    const fetchedPhotos = await fetchPhotos(page);
+
+    if (fetchedPhotos.type === "error") {
+      return { props: { error: fetchedPhotos.errors } };
+    }
+
+    if (fetchedPhotos.type === "success") {
+      setPhotos((data) => [...data, ...fetchedPhotos.response.results]);
+    }
+  };
+
   return (
     <Container>
       {error && <h4>{error}</h4>}
-      {photos &&
-        photos.map((photo, i) => (
-          <Image
-            key={i}
-            width={photo.width}
-            height={photo.height}
-            src={photo.urls.regular}
-            alt={photo.alt_description}
-          />
-        ))}
+      {photos && (
+        <InfiniteScroll
+          dataLength={photos.length}
+          next={() => fetchNextPage(setPage(page + 1))}
+          hasMore={hasMore}
+          loader={<h4 style={{ textAlign: "center" }}>Loading...</h4>}
+          endMessage={
+            <p style={{ textAlign: "center" }}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }
+        >
+          {photos &&
+            photos.map((photo, i) => (
+              <Image
+                key={i}
+                width={photo.width}
+                height={photo.height}
+                src={photo.urls.regular}
+                alt={photo.alt_description}
+              />
+            ))}
+        </InfiniteScroll>
+      )}
     </Container>
   );
 }
--- gridy/pages/api/unsplash.js
+++ gridy/pages/api/unsplash.js
@@ -4,7 +4,7 @@ export const unsplash = createApi({
   accessKey: process.env.NEXT_PUBLIC_UNSPLASH_API_ACCESS_KEY,
 });
 
-export async function fetchPhotos() {
-  const response = await unsplash.photos.list({ page: 2 });
+export async function fetchPhotos(page) {
+  const response = await unsplash.photos.list({ page });
   return response;
 }
